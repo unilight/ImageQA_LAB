c3ff7380fb6337a030ad6ee3808d315d3224a997
unilight <vincenthuang316@gmail.com>
2016-12-01 16:35:30 +0800

    Modified model class name; modified train.py

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import math
 
-class Vis_lstm_model:
-	def init_weight(self, dim_in, dim_out, name=None, stddev=1.0):
+class Model:
+	def random_weight(self, dim_in, dim_out, name=None, stddev=1.0):
 		return tf.Variable(tf.truncated_normal([dim_in, dim_out], stddev=stddev/math.sqrt(float(dim_in))), name=name)
 
-	def init_bias(self, dim_out, name=None):
-		return tf.Variable(tf.zeros([dim_out]), name=name)
+	def random_bias(self, dim, name=None):
+		return tf.Variable(tf.zeros([dim]), name=name)
 
 	def __init__(self, options):
 		with tf.device('/cpu:0'):
