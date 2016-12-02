53d5d7fca7813d8bec59aae8019fd5a1b6b3f00e
unilight <vincenthuang316@gmail.com>
2016-12-03 03:47:28 +0800

    Implemented lstm cell

--- train.py
+++ train.py
@@ -45,17 +45,14 @@ def main():
 	}
 	
 	# tf graph input
-	
-	# image_f = tf.placeholder('float32',[ None, modOpts['img_feature_length'] ], name = 'img')
-	# question = tf.placeholder('float32',[None,modOpts['lstm_steps'] - 1], name = "question")
-
 	# (batch_size, 56, 512)
 	lstm_input = tf.placeholder(tf.float32, [None, modOpts['lstm_steps']-1, modOpts['embedding_size']])
-
-	answer = tf.placeholder('float32', [None, modOpts['ans_vocab_size']], name = "answer")
+	# (batch_size, 431)
+	lstm_answer = tf.placeholder(tf.float32, [None, modOpts['ans_vocab_size']], name = "answer")
 
 	# word embedding
-	word_embedding = tf.Variable(tf.random_uniform([modOpts['q_vocab_size'] + 1, modOpts['embedding_size']],
+	# (9738, 512)
+	word_embedding = tf.Variable(tf.random_uniform([modOpts['q_vocab_size'], modOpts['embedding_size']],
 		-1.0, 1.0), name = 'Wemb')
 	
 
@@ -63,21 +60,25 @@ def main():
 	weights = {
 	# (4096, 512)
 	'img_in': tf.Variable(tf.random_normal([modOpts['img_feature_length'], modOpts['embedding_size']])),
-	# (128, 10)
-	'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+	# (512, 512)
+	'input_hidden': tf.Variable(tf.random_normal([modOpts['embedding_size'], modOpts['rnn_size']])),
+	# (512, 431)
+	'hidden_output': tf.Variable(tf.random_normal([modOpts['rnn_size'], modOpts['ans_vocab_size']])
 	}
 
 	biases = {
 	# (512, )
 	'img_in': tf.Variable(tf.constant(0.1, shape=[modOpts['embedding_size'], ])),
 
-	# (128, )
-	# 'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+	# (512, )
+	'input_hidden': tf.Variable(tf.constant(0.1, shape=[modOpts['rnn_size'], ])),
 	
 	# (431, )
 	'out': tf.Variable(tf.constant(0.1, shape=[modOpts['ans_vocab_size'], ]))
 	}
 
+	'''
+	#TODO : these should be processed somewhere else
 	lstm_input = []
 	image_emb = tf.matmul(image_f, weights['img_in']) + biases['img_in']
 	image_emb = tf.nn.tanh(image_embed)
@@ -88,24 +89,37 @@ def main():
 		word_emb = tf.nn.embedding_lookup(word_embedding, question[:,i])
 		word_emb = tf.nn.dropout(word_emb, modOpts['word_emb_dropout'], name = "word_emb" + str(i))
 		lstm_input.append(word_emb)
+	'''
 
-	def LSTM(opts, weights, biases):
+	def LSTM(opts, X, weights, biases):
 
 		# input to cell
 		###########################
+		# transpose the inputs shape from
+		# X ==> (batch_size * 56 steps, 512 inputs)
+		X = tf.reshape(X, [-1, opts['embedding_size']])
 
-		
+		# into hidden
+		# X_in = (batch_size * 56 steps, 512 hidden)
+		X_in = tf.matmul(X, weights['input_hidden']) + biases['input_hiddwn']
+		# X_in ==> (batch_size, 56 steps, 512 hidden)
+		X_in = tf.reshape(X_in, [-1, opts['lstn_steps']-1, opts['rnn_size']])
 
 		# cell
 		###########################
+		# basic LSTM Cell.
+		lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(opts['rnn_size'], forget_bias=1.0, state_is_tuple=True)
+		# lstm cell is divided into two parts (c_state, h_state)
+		init_state = lstm_cell.zero_state(opts['batch_size'], dtype=tf.float32)
+		outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state, time_major=False)
 
 		# output as the final results
 		###########################
-		results = tf.matmul(final_state[1], weights['out']) + biases['out']
+		results = tf.matmul(final_state[1], weights['hidden_output']) + biases['hidden_output']
 
 		return results
 
-	pred = LSTM(modOpts, weights, biases)
+	pred = LSTM(modOpts, lstm_input, weights, biases)
 
 	lstm_model = model.Model(modOpts)
 	input_tensors, t_loss, t_accuracy, t_p = lstm_model.build_model()
