641d66c27bea782df4c65925f83d173a7c51664b
黃文勁 <b03902012@hao-login.localdomain>
2017-03-02 15:27:27 +0800

    Refactor so that model and train are seperated

--- model.py
+++ model.py
@@ -12,42 +12,42 @@ class Model:
 		self.options = options
 
 		# Weights
-		self.Wword_emb = self.random_weight(options.q_vocab_size, options.embedding_size, name = 'WWord_emb')
-		self.Wimg_emb = self.random_weight(options.img_feature_length, options.embedding_size, name = 'WImg_emb')
-		self.Wemb_hidden = self.random_weight(options.embedding_size, options.rnn_size, name = 'Wemb_hidden')
-		self.Whidden_ans = self.random_weight(options.rnn_size, options.ans_vocab_size, name = 'Whidden_ans')
+		self.Wword_emb = self.random_weight(options['q_vocab_size'], options['embedding_size'], name = 'WWord_emb')
+		self.Wimg_emb = self.random_weight(options['img_feature_length'], options['embedding_size'], name = 'WImg_emb')
+		self.Wemb_hidden = self.random_weight(options['embedding_size'], options['rnn_size'], name = 'Wemb_hidden')
+		self.Whidden_ans = self.random_weight(options['rnn_size'], options['ans_vocab_size'], name = 'Whidden_ans')
 
 		# Biases
-		self.Bimg_emb = self.random_bias(options.embedding_size, name = 'BImg_emb')
-		self.Bemb_hidden = self.random_bias(options.rnn_size, name = 'BEmb_hidden')
-		self.Bhidden_ans = self.random_bias(options.ans_vocab_size, name = 'BHidden_ans')
+		self.Bimg_emb = self.random_bias(options['embedding_size'], name = 'BImg_emb')
+		self.Bemb_hidden = self.random_bias(options['rnn_size'], name = 'BEmb_hidden')
+		self.Bhidden_ans = self.random_bias(options['ans_vocab_size'], name = 'BHidden_ans')
 
-	def build_model():
-		lstm_image_feat = tf.placeholder(tf.float32, [self.options.batch_size, self.options.img_feature_length])
-		lstm_q_vec = tf.placeholder(tf.float32, [self.batch_size, self.options.lstm_steps-1, 512])
-		lstm_answer = tf.placeholder(tf.float32, [self.options.batch_size, self.options.ans_vocab_size], name = "answer")
+	def build_model(self):
+		lstm_image_feat = tf.placeholder(tf.float32, [self.options['batch_size'], self.options['img_feature_length']])
+		lstm_q_vec = tf.placeholder(tf.float32, [self.options['batch_size'], self.options['lstm_steps']-1, 512])
+		lstm_answer = tf.placeholder(tf.float32, [self.options['batch_size'], self.options['ans_vocab_size']], name = "answer")
 
 		# LSTM preprocessing
-		image_emb = tf.matmul(lstm_image_feat, Wimg_emb) + Bimg_emb
+		image_emb = tf.matmul(lstm_image_feat, self.Wimg_emb) + self.Bimg_emb
 		image_emb = tf.nn.tanh(image_emb)
-		image_emb = tf.nn.dropout(image_emb, self.options.image_dropout, name = "Dropout")
+		image_emb = tf.nn.dropout(image_emb, self.options['image_dropout'], name = "Dropout")
 		image_emb = tf.reshape(image_emb, [-1, 1, 512])	
 		q_emb = tf.reshape(lstm_q_vec, [-1, 10, 512])
 		# Concat question to image
 		X = tf.concat(1, [image_emb, q_emb], name = "finalX")
-		X = tf.reshape(X, [-1, self.options.embedding_size])
+		X = tf.reshape(X, [-1, self.options['embedding_size']])
 
-		X_in = tf.matmul(X, Wemb__hidden) + Bemb_hidden
-		X_in = tf.reshape(X_in, [-1, self.options.lstm_steps, self.options.rnn_size])
+		X_in = tf.matmul(X, self.Wemb_hidden) + self.Bemb_hidden
+		X_in = tf.reshape(X_in, [-1, self.options['lstm_steps'], self.options['rnn_size']])
 
 		# LSTM
-		lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.options.rnn_size, forget_bias=1.0, state_is_tuple=True)
+		lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.options['rnn_size'], forget_bias=1.0, state_is_tuple=True)
 		# lstm cell is divided into two parts (c_state, h_state)
 		cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * 2, state_is_tuple=True)
-		init_state = cell.zero_state(self.options.batch_size, dtype=tf.float32)
+		init_state = cell.zero_state(self.options['batch_size'], dtype=tf.float32)
 		outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
 
-		logits = tf.matmul(final_state[1][1], Whidden_ans) + Bhidden_ans
+		logits = tf.matmul(final_state[1][1], self.Whidden_ans) + self.Bhidden_ans
 		ce = tf.nn.softmax_cross_entropy_with_logits(logits, lstm_answer)
 		answer_probab = tf.nn.softmax(logits)
 
@@ -58,7 +58,7 @@ class Model:
 		
 		input_tensors = {
 			'image' : lstm_image_feat,
-			'sentence' : lsem_q_vec,
+			'sentence' : lstm_q_vec,
 			'answer' : lstm_answer
 		} #
 		return input_tensors, loss, accuracy, predictions
