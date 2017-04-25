4acc69b0d1d4133bbcad291584d107f4cfb51ed8
邵楚荏 <nickshao@hao-login.localdomain>
2017-04-25 13:17:00 +0800

    add testdata load()

--- parse.py
+++ parse.py
@@ -27,9 +27,29 @@ def load_question_answer(opts):
         'answer_vocab' : ans_vocab, 
         'question_vocab' : ques_vocab
         }
-    print("Data",len(data['training_data']))
+    print("Training Data",len(data['training_data']))
     return data
-
+def loadtest_question_answer(opts):
+    load_data = np.load('./cocoqa/test.npy',encoding='bytes')
+    testing_data = []
+    for data_id in range(len(load_data[0])-42):
+        testing_data.append({
+        'image_id' : load_data[0][data_id][0][0], 
+        'question' : np.zeros(55),
+        'answer' : load_data[1][data_id][0]
+        })
+        for question_id in range(55):
+            testing_data[-1]['question'][question_id] = load_data[0][data_id][question_id+1][0]
+    ques_vocab = get_ques_vocab('./cocoqa/vocab-dict.npy')
+    ans_vocab = get_answer_vocab('./cocoqa/ansdict.pkl')
+    data = {
+        'testing_data' : testing_data,
+        'max_question_length' : 55,
+        'answer_vocab' : ans_vocab,
+        'question_vocab' : ques_vocab
+        }
+    print("Testing Data", len(data['testing_data']))
+    return data
 def get_ques_vocab(data_dir):
     load_ques_data = np.load(data_dir, encoding='bytes')
     #print load_ques_data[1][0] 
